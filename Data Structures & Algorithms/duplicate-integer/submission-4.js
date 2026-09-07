71827c818b099efc3e58e63a711243d5cde1f420
Amandeep Takhi <amantakhi@gmail.com>
2026-09-07 14:21:18 -0400

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        //brute force O(n2), O(1)
+        // for(var i=0;i<nums.length;i++) {
+        //     for(var j=0;j<nums.length;j++){
+        //         if(i!=j) {
+        //             if(nums[i]==nums[j]) {
+        //                 return true;
+        //             }
+        //         }
+        //     }
+        // }
+        // return false;
+
+        //sorting? O(nlogn) O(1)
+        nums = nums.sort();
+        for(var i=0;i<nums.length - 1;i++) {
+            if(nums[i] == nums[i+1])
+                return true;
+        }
+        return false;
+    }
+}
