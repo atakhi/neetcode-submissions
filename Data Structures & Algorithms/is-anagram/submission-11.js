77cf881eb4a002d3e135fb7c7378f58a8bf77250
Amandeep Takhi <amantakhi@gmail.com>
2026-09-10 14:27:59 -0400

    Add: is-anagram - submission-11

---
+++ Data Structures & Algorithms/is-anagram/submission-11.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        let mp = {};
+        if(s.length != t.length) {
+            return false;
+        }
+        s = s.split('').sort().join();
+        t = t.split('').sort().join();
+        
+        for(var i=0;i<s.length;i++) {
+            if(s[i] != t[i]){ 
+                return false;
+            }
+        }
+        return true;
+    }
+}
