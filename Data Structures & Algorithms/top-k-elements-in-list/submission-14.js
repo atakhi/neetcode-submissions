21d3eb2c3652684df57f168a5033f01f8105f710
Amandeep Takhi <amantakhi@gmail.com>
2026-09-24 17:10:13 -0400

    Add: top-k-elements-in-list - submission-14

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-14.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        var arr = Array.from({length: nums.length + 1}, () => []);
+        var mp={};
+        for(var n of nums) {
+            mp[n] = (mp[n] || 0) + 1;
+        }
+        for(var obj in mp) {
+            arr[mp[obj]].push(obj);
+        }
+        let res = [];
+        for(var j=arr.length - 1;j>=0;j--) {
+            for(var n of arr[j]){
+                res.push(n);
+                if(res.length == k) {
+                    return res;
+                }
+            }
+        }
+        return res;
+    }
+}
